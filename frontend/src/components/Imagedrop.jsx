82b17d92c6f0a143fbeefa3931b23db923ef2d03
AvinashNayak27 <Avinashnayak573@gmail.com>
2023-12-09 23:02:26 +0530

    Add camera functionality to ImageDrop component

--- frontend/src/components/Imagedrop.jsx
+++ frontend/src/components/Imagedrop.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 import { ethers } from "ethers";
 import CryptoJS from "crypto-js";
 import lighthouse from "@lighthouse-web3/sdk";
@@ -16,6 +16,8 @@ const ImageDrop = () => {
   const [cid, setCid] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
 
   const onDrop = useCallback((event) => {
     event.preventDefault();
@@ -189,6 +191,60 @@ const ImageDrop = () => {
       toast.error(error.reason);
     }
   };
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+
+  const openCamera = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+      if (videoRef.current) {
+        setIsCameraOpen(true);
+        videoRef.current.srcObject = stream;
+        videoRef.current.style.display = "block"; // Make the video element visible
+      }
+    } catch (error) {
+      console.error("Error accessing the camera:", error);
+      setErrorMessage("Error accessing the camera.");
+    }
+  };
+
+  const captureImage = () => {
+    if (videoRef.current && canvasRef.current) {
+      const context = canvasRef.current.getContext("2d");
+      context.drawImage(
+        videoRef.current,
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      const imageDataUrl = canvasRef.current.toDataURL("image/png");
+
+      setImage(imageDataUrl); // Set the captured image for preview
+      const file = dataURLtoFile(imageDataUrl, "captured-image.png");
+      processFile([file]);
+      // close camera
+
+      if (videoRef.current.srcObject) {
+        videoRef.current.srcObject.getTracks().forEach((track) => track.stop());
+        videoRef.current.style.display = "none"; // Hide the video element again
+      }
+    }
+  };
+
+  const dataURLtoFile = (dataurl, filename) => {
+    let arr = dataurl.split(","),
+      mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]),
+      n = bstr.length,
+      u8arr = new Uint8Array(n);
+
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new File([u8arr], filename, { type: mime });
+  };
 
   return (
     <div className="flex flex-col items-center p-4">
@@ -202,8 +258,37 @@ const ImageDrop = () => {
 
       {account && (
         <div className="w-full max-w-md">
+          <button
+            onClick={openCamera}
+            className="mb-6 bg-yellow-500 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-opacity-50 active:bg-yellow-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out"
+          >
+            Open Camera
+          </button>
+          <video
+            ref={videoRef}
+            autoPlay
+            style={{ width: "640px", height: "480px", display: "none" }}
+            className="rounded shadow-lg"
+          ></video>
+          <canvas
+            ref={canvasRef}
+            style={{ display: "none" }}
+            width="640"
+            height="480"
+            className="rounded shadow-lg"
+          ></canvas>
+          {
+            isCameraOpen && (
+              <button
+              onClick={captureImage}
+              className="mb-6 bg-green-500 hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 active:bg-green-700 text-white font-bold py-2 px-4 rounded transition duration-150 ease-in-out ml-4"
+            >
+              Capture Image
+            </button> )
+          }
+
           <div
-            className="border-2 border-dashed border-gray-300 rounded-md p-6 text-center cursor-pointer mb-6"
+            className="border-2 border-dashed border-gray-300 rounded-md p-6 text-center cursor-pointer mb-6 hover:border-gray-400 focus:outline-none focus:border-gray-500 transition duration-150 ease-in-out"
             onClick={openFileDialog}
             onDrop={onDrop}
             onDragOver={onDragOver}
@@ -215,6 +300,7 @@ const ImageDrop = () => {
               onChange={onFileChange}
               className="hidden"
             />
+
             {image ? (
               <>
                 <img
